3433aa47b67c9cc384adc8bce36fc0623b43555d
csterling <coreytsterling@gmail.com>
2020-01-16 13:17:03 +1300

    Added the members of teams to the team's serialisation.

--- ufdl-core-app/src/ufdl/core_app/serialisers/_TeamSerialiser.py
+++ ufdl-core-app/src/ufdl/core_app/serialisers/_TeamSerialiser.py
@@ -1,9 +1,13 @@
+from rest_framework import serializers
 from simple_django_teams.models import Team
 
 from .mixins import SoftDeleteModelSerialiser
 
 
 class TeamSerialiser(SoftDeleteModelSerialiser):
+    # Members has to be explicitly specified to use the slug
+    members = serializers.SlugRelatedField("username", many=True, read_only=True)
+
     class Meta:
         model = Team
-        fields = ["name"] + SoftDeleteModelSerialiser.base_fields
+        fields = ["name", "members"] + SoftDeleteModelSerialiser.base_fields

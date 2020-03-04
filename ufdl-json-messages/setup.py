f9fe6dcf3c07143da9eb91eef694d713039c22a1
csterling <coreytsterling@gmail.com>
2020-03-05 12:03:29 +1300

    Implemented setup script.

---
+++ ufdl-json-messages/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+# Copyright (C) 2020 Fracpete (fracpete at waikato dot ac dot nz)
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="ufdl.json",
+    description="Definitions of JSON messages used by UFDL",
+    url="https://github.com/waikato-ufdl/ufdl-backend",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3',
+    ],
+    license='Apache License Version 2.0',
+    package_dir={
+        '': 'src'
+    },
+    packages=find_namespace_packages(where='src'),
+    namespace_packages=[
+        "ufdl"
+    ],
+    version="0.0.1",
+    author='Corey Sterling',
+    author_email='coreytsterling@gmail.com',
+    install_requires=[
+        "wai.json"
+    ]
+)

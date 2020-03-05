096546b76c84c29b97aaeea38e2743363c901593
csterling <coreytsterling@gmail.com>
2020-03-06 11:02:56 +1300

    Added utility method for getting the value of a parameter, or raising an error if it's missing.

--- ufdl-core-app/src/ufdl/core_app/exceptions/_MissingParameter.py
+++ ufdl-core-app/src/ufdl/core_app/exceptions/_MissingParameter.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.exceptions import APIException
 
+from ..util import QueryParameters, QueryParameterValue
+
 
 class MissingParameter(APIException):
     """
@@ -11,3 +13,11 @@ class MissingParameter(APIException):
 
     def __init__(self, name: str):
         super().__init__(f"Required parameter '{name}' is missing")
+
+    @staticmethod
+    def attempt_pop(name: str, parameters: QueryParameters) -> QueryParameterValue:
+        # Pop and return the parameter if present
+        if name in parameters:
+            return parameters.pop(name)
+
+        raise MissingParameter(name)

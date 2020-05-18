22b761c176505943884d17d7aa8731ceba4daf88
csterling <coreytsterling@gmail.com>
2020-05-18 13:22:02 +1200

    JSON messages package moved to its own repo.

--- ufdl-json-messages/src/ufdl/json/image_classification/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Package for definitions of JSON formats.
-"""
-from ._CategoriesFile import CategoriesFile
-from ._CategoriesModSpec import CategoriesModSpec
--- ufdl-json-messages/src/ufdl/json/object_detection/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package for definitions of JSON formats.
-"""
-from ._Annotation import Annotation
-from ._AnnotationsFile import AnnotationsFile
-from ._AnnotationsModSpec import AnnotationsModSpec
-from ._Image import Image
-from ._Polygon import Polygon

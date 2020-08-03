df38b5b957012ab809e8bac3e0dc2cfc8085e7db
Peter Reutemann <fracpete@gmail.com>
2020-08-03 15:39:36 +1200

    adapted to make it work with Python 3.8 as well

--- dev_init.sh
+++ dev_init.sh
@@ -2,19 +2,30 @@
 
 function check_executable()
 {
+  echo "Checking $EXEC..."
   if [ ! -x "/usr/bin/$EXEC" ]
   then
-    echo	  
-    echo "$EXEC executable not present!"
-    echo "Install on Debian systems with:"
-    echo "sudo apt-get install $EXEC $ADDITIONAL"
-    echo	  
-    exit 1
+    AVAILABLE=false
+    if [ "$REQUIRED" = "true" ]
+    then
+      echo
+      echo "$EXEC executable not present!"
+      echo "Install on Debian systems with:"
+      echo "sudo apt-get install $EXEC $ADDITIONAL"
+      echo
+      exit 1
+    else
+      echo "...NOT present"
+    fi
+  else
+    echo "...is present"
+    AVAILABLE=true
   fi
 }
 
 function check_repository()
 {
+  echo "Checking repo $REPO..."
   if [ ! -d "../$REPO" ]
   then
     echo	  
@@ -24,20 +35,59 @@ function check_repository()
     echo "git clone https://github.com/waikato-ufdl/$REPO.git"
     echo	  
     exit 2
+  else
+    echo "...is present"
   fi
 }
 
+echo "Performing checks"
+
 EXEC="virtualenv"
 ADDITIONAL=""
+REQUIRED=true
 check_executable
+
 EXEC="python3.7"
 ADDITIONAL="python3.7-dev"
+REQUIRED=false
 check_executable
+PYTHON37_AVAILABLE=$AVAILABLE
+
+EXEC="python3.8"
+ADDITIONAL="python3.8-dev"
+REQUIRED=false
+check_executable
+PYTHON38_AVAILABLE=$AVAILABLE
+
+if [ "$PYTHON37_AVAILABLE" = "false" ] && [ "$PYTHON38_AVAILABLE" = "false" ]
+then
+  echo
+  echo "Neither Python 3.7 nor Python3.8 are available!"
+  echo "Install on Debian systems with:"
+  echo "  sudo apt-get install python3.7 python3.7-dev"
+  echo "or"
+  echo "  sudo apt-get install python3.8 python3.8-dev"
+  echo
+  exit 1
+fi
+
+if [ "$PYTHON37_AVAILABLE" = "true" ]
+then
+  PYTHON=python3.7
+elif [ "$PYTHON38_AVAILABLE" = "true" ]
+then
+  PYTHON=python3.8
+else
+  echo "Don't know what Python executable to use!"
+  exit 2
+fi
+
 
 REPO="ufdl-json-messages"
 check_repository
 
-echo "Press any key to start setup of 'venv.dev' for running UFDL backend"
+echo
+echo "Press any key to start setup of 'venv.dev' for running UFDL backend (using $PYTHON)..."
 read -s -n 1 key
 
 # delete old directory
@@ -48,7 +98,7 @@ then
 fi
 
 echo "Creating new virtual environment..."
-virtualenv -p /usr/bin/python3.7 ./venv.dev
+virtualenv -p /usr/bin/$PYTHON ./venv.dev
 
 echo "Installing dependencies..."
 ./venv.dev/bin/pip install --upgrade pip

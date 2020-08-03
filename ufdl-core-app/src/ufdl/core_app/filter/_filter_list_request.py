b4a938bcfa313db2131034b95b18228c7ada1164
csterling <coreytsterling@gmail.com>
2020-08-03 15:04:46 +1200

    Added option to filtering to include soft-deleted objects, and exclude them by default.

--- ufdl-core-app/src/ufdl/core_app/filter/_filter_list_request.py
+++ ufdl-core-app/src/ufdl/core_app/filter/_filter_list_request.py
@@ -1,9 +1,12 @@
 from django.db.models import QuerySet
 
+from simple_django_teams.mixins import SoftDeleteQuerySet
+
 from ufdl.json.core.filter import FilterSpec
 
 from wai.json.object import Absent
 
+from ..models import User
 from ._generate_qs import generate_qs
 from ._order_by import order_by
 
@@ -24,5 +27,12 @@ def filter_list_request(query_set: QuerySet, filter_spec: FilterSpec) -> QuerySe
     # If order-by expressions are provided, order by them
     if filter_spec.order_by is not Absent:
         query_set = order_by(query_set, filter_spec.order_by)
+
+    # If not requested to leave inactive instances in, remove them
+    if not filter_spec.include_inactive:
+        if query_set.model is User:
+            query_set = query_set.filter(is_active=True)
+        elif isinstance(query_set, SoftDeleteQuerySet):
+            query_set = query_set.active()
 
     return query_set

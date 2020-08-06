6abfe3e571f99b2a305e356d9b8d326268fbf6ad
csterling <coreytsterling@gmail.com>
2020-08-07 09:46:36 +1200

    Created view-set for jobs.

---
+++ ufdl-core-app/src/ufdl/core_app/views/jobs/_JobViewSet.py
@@ -0,0 +1,17 @@
+from ...models.jobs import Job
+from ...serialisers.jobs import JobSerialiser
+from ...permissions import IsAuthenticated, IsAdminUser
+from ..mixins import SoftDeleteViewSet, AddJobOutputViewSet
+from .._UFDLBaseViewSet import UFDLBaseViewSet
+
+
+class JobViewSet(AddJobOutputViewSet, SoftDeleteViewSet, UFDLBaseViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerialiser
+
+    admin_permission_class = IsAdminUser
+
+    permission_classes = {
+        "list": [IsAuthenticated],
+        "retrieve": [IsAuthenticated]
+    }
--- ufdl-core-app/src/ufdl/core_app/views/jobs/__init__.py
+++ ufdl-core-app/src/ufdl/core_app/views/jobs/__init__.py
@@ -1,1 +1,3 @@
+from ._JobTemplateViewSet import JobTemplateViewSet
 from ._JobTypeViewSet import JobTypeViewSet
+from ._JobViewSet import JobViewSet

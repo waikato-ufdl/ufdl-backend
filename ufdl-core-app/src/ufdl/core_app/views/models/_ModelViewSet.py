92453c40315e24e9fbf6c352099e9a28478cf1f4
csterling <coreytsterling@gmail.com>
2020-08-07 11:27:46 +1200

    Added soft-delete end-points to models.

--- ufdl-core-app/src/ufdl/core_app/views/models/_ModelViewSet.py
+++ ufdl-core-app/src/ufdl/core_app/views/models/_ModelViewSet.py
@@ -1,11 +1,11 @@
 from ...models.models import Model
 from ...serialisers.models import ModelSerialiser
 from ...permissions import IsAdminUser, IsAuthenticated
-from ..mixins import DownloadableViewSet, SetFileViewSet
+from ..mixins import DownloadableViewSet, SetFileViewSet, SoftDeleteViewSet
 from .._UFDLBaseViewSet import UFDLBaseViewSet
 
 
-class ModelViewSet(SetFileViewSet, DownloadableViewSet, UFDLBaseViewSet):
+class ModelViewSet(SetFileViewSet, DownloadableViewSet, SoftDeleteViewSet, UFDLBaseViewSet):
     queryset = Model.objects.all()
     serializer_class = ModelSerialiser
 

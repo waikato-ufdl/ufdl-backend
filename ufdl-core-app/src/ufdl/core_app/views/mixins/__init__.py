c460f53389a1fe0d1f40778f52237d1fa8a62c43
csterling <coreytsterling@gmail.com>
2020-08-06 16:02:33 +1200

    Added view-set for job templates, with mixin for managing inputs/parameters.

--- ufdl-core-app/src/ufdl/core_app/views/mixins/__init__.py
+++ ufdl-core-app/src/ufdl/core_app/views/mixins/__init__.py
@@ -4,6 +4,7 @@ Package of mixin functionality for view-sets.
 from ._CopyableViewSet import CopyableViewSet
 from ._DownloadableViewSet import DownloadableViewSet
 from ._FileContainerViewSet import FileContainerViewSet
+from ._InputsParametersViewSet import InputsParametersViewSet
 from ._LicenceSubdescriptorViewSet import LicenceSubdescriptorViewSet
 from ._MembershipViewSet import MembershipViewSet
 from ._RoutedViewSet import RoutedViewSet

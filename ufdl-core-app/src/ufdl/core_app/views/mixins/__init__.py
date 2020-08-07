bb0321cb6faa1dcb4f1762ba062023beb3237f88
csterling <coreytsterling@gmail.com>
2020-08-07 13:32:02 +1200

    Created view-set for creating jobs from templates.

--- ufdl-core-app/src/ufdl/core_app/views/mixins/__init__.py
+++ ufdl-core-app/src/ufdl/core_app/views/mixins/__init__.py
@@ -3,6 +3,7 @@ Package of mixin functionality for view-sets.
 """
 from ._AddJobOutputViewSet import AddJobOutputViewSet
 from ._CopyableViewSet import CopyableViewSet
+from ._CreateJobViewSet import CreateJobViewSet
 from ._DownloadableViewSet import DownloadableViewSet
 from ._FileContainerViewSet import FileContainerViewSet
 from ._InputsParametersViewSet import InputsParametersViewSet

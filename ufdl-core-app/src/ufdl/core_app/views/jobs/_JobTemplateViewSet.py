eb3dc70e59494fe230dda1b06b7dd8d15ba1bfba
csterling <coreytsterling@gmail.com>
2020-08-07 13:40:19 +1200

    Added the create-job end-point to the job template view-set.

--- ufdl-core-app/src/ufdl/core_app/views/jobs/_JobTemplateViewSet.py
+++ ufdl-core-app/src/ufdl/core_app/views/jobs/_JobTemplateViewSet.py
@@ -1,11 +1,11 @@
 from ...models.jobs import JobTemplate
 from ...serialisers.jobs import JobTemplateSerialiser
 from ...permissions import IsAuthenticated, IsAdminUser
-from ..mixins import SoftDeleteViewSet, InputsParametersViewSet
+from ..mixins import SoftDeleteViewSet, InputsParametersViewSet, CreateJobViewSet
 from .._UFDLBaseViewSet import UFDLBaseViewSet
 
 
-class JobTemplateViewSet(InputsParametersViewSet, SoftDeleteViewSet, UFDLBaseViewSet):
+class JobTemplateViewSet(CreateJobViewSet, InputsParametersViewSet, SoftDeleteViewSet, UFDLBaseViewSet):
     queryset = JobTemplate.objects.all()
     serializer_class = JobTemplateSerialiser
 

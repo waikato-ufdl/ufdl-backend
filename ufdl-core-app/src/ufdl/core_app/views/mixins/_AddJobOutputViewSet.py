e85112c2c70be3630037cc97af94c28e17132584
csterling <coreytsterling@gmail.com>
2020-08-10 10:55:46 +1200

    Made the type of the job output part of the URL.

--- ufdl-core-app/src/ufdl/core_app/views/mixins/_AddJobOutputViewSet.py
+++ ufdl-core-app/src/ufdl/core_app/views/mixins/_AddJobOutputViewSet.py
@@ -5,9 +5,7 @@ from rest_framework.parsers import FileUploadParser
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from ufdl.json.core.jobs import JobOutputTypeSpec
-
-from ...exceptions import JSONParseFailure, BadName
+from ...exceptions import BadName
 from ...models.files import File
 from ...models.jobs import Job, JobOutput
 from ...renderers import BinaryFileRenderer
@@ -27,9 +25,8 @@ class AddJobOutputViewSet(RoutedViewSet):
     def get_routes(cls) -> List[routers.Route]:
         return [
             routers.Route(
-                url=r'^{prefix}/{lookup}/outputs/(?P<name>.+)$',
+                url=r'^{prefix}/{lookup}/outputs/(?P<name>[^/]+)/?(?P<type>[^/]*)$',
                 mapping={'post': 'add_output',
-                         'patch': 'set_output_type',
                          'delete': 'delete_output',
                          'get': 'get_output'},
                 name='{basename}-job-outputs',
@@ -52,13 +49,14 @@ class AddJobOutputViewSet(RoutedViewSet):
 
         return [BinaryFileRenderer()]
 
-    def add_output(self, request: Request, pk=None, name=None):
+    def add_output(self, request: Request, pk=None, name=None, type=None):
         """
         Action to add an output to a job.
 
         :param request:     The request containing the output data.
         :param pk:          The primary key of the job.
         :param name:        The name of the output.
+        :param type:        The type of the output.
         :return:            The response containing the output.
         """
         # Get the job the output is being added to
@@ -68,50 +66,27 @@ class AddJobOutputViewSet(RoutedViewSet):
         if job.outputs.filter(name=name).exists():
             raise BadName(name, "Job already has an output by this name")
 
+        # If no type is given, use the empty string
+        if type is None:
+            type = ""
+
         # Read the data from the request
         data = request.data['file'].file.read()
 
         # Create the output
-        output = JobOutput(job=job, name=name, type="", data=File.get_reference_from_backend(data), creator=request.user)
+        output = JobOutput(job=job, name=name, type=type, data=File.get_reference_from_backend(data), creator=request.user)
         output.save()
 
         return Response(JobOutputSerialiser().to_representation(output))
 
-    def set_output_type(self, request: Request, pk=None, name=None):
+    def delete_output(self, request: Request, pk=None, name=None, type=None):
         """
         Action to set the type of an output to a job.
 
         :param request:     The request containing the output type.
         :param pk:          The primary key of the job.
         :param name:        The name of the output.
-        :return:            The response containing the output.
-        """
-        # Get the job the output belongs to
-        job = self.get_object_of_type(Job)
-
-        # Get the named output
-        output = job.outputs.filter(name=name).first()
-
-        # Make sure the output exists
-        if output is None:
-            raise BadName(name, "Job has no output by this name")
-
-        # Parse the type from the request
-        spec = JSONParseFailure.attempt(dict(request.data), JobOutputTypeSpec)
-
-        # Update the output's type
-        output.type = spec.type
-        output.save(update_fields=["type"])
-
-        return Response(JobOutputSerialiser().to_representation(output))
-
-    def delete_output(self, request: Request, pk=None, name=None):
-        """
-        Action to set the type of an output to a job.
-
-        :param request:     The request containing the output type.
-        :param pk:          The primary key of the job.
-        :param name:        The name of the output.
+        :param type:        The type of the output.
         :return:            The response containing the output.
         """
         # Get the job the output belongs to
@@ -129,13 +104,14 @@ class AddJobOutputViewSet(RoutedViewSet):
 
         return Response(JobOutputSerialiser().to_representation(output))
 
-    def get_output(self, request: Request, pk=None, name=None):
+    def get_output(self, request: Request, pk=None, name=None, type=None):
         """
         Action to get the data from a job output.
 
         :param request:     The request.
         :param pk:          The primary key of the job.
         :param name:        The name of the output.
+        :param type:        The type of the output.
         :return:            The response containing the output data.
         """
         # Get the job the output belongs to

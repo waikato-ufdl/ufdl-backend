56842a137bb7dbd98c7176bc2d39862c1644ddd1
Peter Reutemann <fracpete@gmail.com>
2020-09-24 08:38:49 +1200

    added output how server is started

--- dev_start.sh
+++ dev_start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./venv.dev/bin/python -m ufdl.api_site.scripts.manage runserver $1
+VENV="venv.dev"
+
+if [ "$1" = "" ]
+then
+  echo "Starting dev server on localhost:8000"
+  echo "Use '0.0.0.0:8000' as first argument to make server available outside of localhost"
+else
+  echo "Starting dev server on $1"
+fi
+
+./$VENV/bin/python -m ufdl.api_site.scripts.manage runserver $1

7eb87b1631c7244e8d4282fa3c917ce324e3ade9
csterling <coreytsterling@gmail.com>
2020-12-04 14:52:32 +1300

    Added an optional reason parameter to BadArgumentValue.

--- ufdl-core-app/src/ufdl/core_app/exceptions/_BadArgumentValue.py
+++ ufdl-core-app/src/ufdl/core_app/exceptions/_BadArgumentValue.py
@@ -12,11 +12,30 @@ class BadArgumentValue(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 'bad_argument_value'
 
-    def __init__(self, action_name: str,
-                 parameter_name: str,
-                 value: str,
-                 allowed_values: Optional[str] = None):
+    def __init__(
+            self,
+            action_name: str,
+            parameter_name: str,
+            value: str,
+            allowed_values: Optional[str] = None,
+            reason: Optional[str] = None,
+    ):
         # Format the allowed values
-        allowed_values = f", options: {allowed_values}" if allowed_values is not None else ""
-        super().__init__(f"Bad value for {action_name} parameter '{parameter_name}': {value}"
-                         f"{allowed_values}")
+        allowed_values = (
+            f", options: {allowed_values}"
+            if allowed_values is not None else
+            ""
+        )
+
+        # Format the reason
+        reason = (
+            ""
+            if reason is None else
+            f"\n{reason}"
+        )
+
+        super().__init__(
+            f"Bad value for {action_name} parameter '{parameter_name}': {value}"
+            f"{allowed_values}"
+            f"{reason}"
+        )

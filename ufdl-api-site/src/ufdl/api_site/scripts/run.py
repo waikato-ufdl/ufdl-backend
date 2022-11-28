8ccfb56f562ebe0769eb462ad870ad2fa2506eb4
csterling <coreytsterling@gmail.com>
2022-11-29 11:31:19 +1300

    Added env var for resetting the database on run.

--- ufdl-api-site/src/ufdl/api_site/scripts/run.py
+++ ufdl-api-site/src/ufdl/api_site/scripts/run.py
@@ -5,6 +5,14 @@ import sys
 
 
 def run(argv=sys.argv):
+    # Check if we need to reset the database first
+    import os
+    should_reset = os.environ.get('UFDL_RESET_BACKEND_ON_RUN', 'false').upper()
+    if should_reset in ('1', 'YES', 'Y', 'TRUE'):
+        from .reset import reset
+        script = argv[0].replace("run.py", "reset.py")
+        reset([script])
+
     from daphne.cli import CommandLineInterface
     sys.exit(CommandLineInterface().run(["ufdl.api_site.asgi:application"] + argv[1:]))
 

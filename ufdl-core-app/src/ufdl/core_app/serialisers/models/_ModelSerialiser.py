56b17149ddb02c54012440cbe221da524e6725ea
csterling <coreytsterling@gmail.com>
2023-04-21 17:24:11 +1200

    Fixed model serialisation.

--- ufdl-core-app/src/ufdl/core_app/serialisers/models/_ModelSerialiser.py
+++ ufdl-core-app/src/ufdl/core_app/serialisers/models/_ModelSerialiser.py
@@ -9,12 +9,16 @@ from ..nodes import FrameworkSerialiser
 
 class ModelSerialiser(SoftDeleteModelSerialiser):
     # Slug fields must be specified explicitly
-    domain = serializers.SlugRelatedField("description", queryset=DataDomain.objects)
-    framework = FrameworkSerialiser()
+    domain = serializers.SlugRelatedField("name", queryset=DataDomain.objects)
     licence = serializers.SlugRelatedField("name", queryset=Licence.objects)
 
     # Getting/setting data is done separately, just indicate if there is data
     data = serializers.BooleanField(read_only=True, source='has_data')
+
+    def to_representation(self, instance: Model):
+        representation = super().to_representation(instance)
+        representation["framework"] = FrameworkSerialiser().to_representation(instance.framework)
+        return representation
 
     class Meta:
         model = Model
